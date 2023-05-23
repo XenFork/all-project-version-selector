867f0c07c6d8d89973275f57abb36a004c263169
baka4n <qq474899581@gmail.com>
2023-05-23 11:27:28 +0800

    test

--- lang.js
+++ lang.js
@@ -2,16 +2,18 @@
     var lang = navigator.language || navigator.userLanguage;
     lang = lang.substr(0, 2);
     if (lang == 'zh') {
-        document.writeln("```chinese\n" +
-            "    所有组织都可使用的项目版本，依赖远程检索器\n" +
-            "    用于在gradle远程获取版本信息最新依赖项的版本信息  \n" +
-            "```\n" +
-            "### 目前授权的组织有：\n" +
-            "- [AmazingRabbit Studio](https://github.com/AmazingRabbit-Studio)\n" +
-            "- [ChinaWareMC](https://github.com/ChinaWareMC)\n" +
-            "- [Funny Developers](https://github.com/Funny-Developers)\n" +
-            "- [Overrun Organization](https://github.com/Over-Run)\n" +
-            "- [SquidCraftTeam](https://github.com/SquidCraftTeam)\n" +
-            "- [氙叉联盟 - XenFork Union](https://github.com/XenFork)")
+        <span>
+            ```chinese
+    所有组织都可使用的项目版本，依赖远程检索器
+    用于在gradle远程获取版本信息最新依赖项的版本信息
+```
+### 目前授权的组织有：
+- [AmazingRabbit Studio](https://github.com/AmazingRabbit-Studio)
+- [ChinaWareMC](https://github.com/ChinaWareMC)
+- [Funny Developers](https://github.com/Funny-Developers)
+- [Overrun Organization](https://github.com/Over-Run)
+- [SquidCraftTeam](https://github.com/SquidCraftTeam)
+- [氙叉联盟 - XenFork Union](https://github.com/XenFork)
+        </span>
     }
 }
